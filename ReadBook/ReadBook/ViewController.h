4340dcfe4a46fc9674cf4eaddaf32f63b447658c
modestfly <wangqifei10@163.com>
2018-08-14 11:35:42 +0800

    TesseractOCR库导入

--- ReadBook/ReadBook/ViewController.h
+++ ReadBook/ReadBook/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <TesseractOCR/TesseractOCR.h>
 @interface ViewController : UIViewController
 
 
--- ReadBook/ReadBook/ViewController.m
+++ ReadBook/ReadBook/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <Masonry.h>
 @interface ViewController ()
 
 @end
@@ -17,7 +17,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
+    [self creatSubViews];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)creatSubViews{
+    
+    UIButton *scanButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    scanButton.frame = CGRectMake(50, 100, 200, 50);
+    [scanButton setTitle:@"文字识别按钮" forState:UIControlStateNormal];
+    [scanButton setBackgroundColor:[UIColor greenColor]];
+    [scanButton addTarget:self action:@selector(scanButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:scanButton];
+    
+    UIButton *faceButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    faceButton.frame = CGRectMake(50, 180, 200, 50);
+    [faceButton setTitle:@"人脸识别按钮" forState:UIControlStateNormal];
+    [faceButton setBackgroundColor:[UIColor greenColor]];
+    [faceButton addTarget:self action:@selector(faceButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:faceButton];
+    
+}
+//文字识别方法
+-(void)scanButtonClick:(UIButton *)sender{
+    //异步开启多线程，不能阻塞主线程的使用
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        //初始化G8Tesseract类，为文字识别做准备
+        G8Tesseract *CardTesseract = [[G8Tesseract alloc] initWithLanguage:@"eng+chi_sim"];
+        
+        
+        
+    });
+    
+}
+//人脸识别方法
+-(void)faceButtonAction:(UIButton *)sender{
+    
 }
 
 
